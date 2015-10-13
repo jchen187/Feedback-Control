76893f63af169281e67f08e9c07a4b07c86f7a2d
Johnny <liljohnnyxp@yahoo.com>
2015-10-13 14:10:46 -0400

    I graphed the ramp response.

--- HW3.m
+++ HW3.m
@@ -5,12 +5,26 @@ close all;
 %%Question 1
 num = [1];
 den = [1 2 2 0];
-sys1 = tf(num,den) 
+sys1 = tf(num,den);     % My transfer function 
 
 figure;
 rlocus(sys1);
+% [k, poles] = rlocfind(sys1);
 
 %Give it a ramp input to see the response
+t = 0:0.1:10;
+alpha = 1;
+ramp = alpha*t;         % My input signal
+[y,t] = lsim(sys1,ramp,t);
+figure;
+plot(t,y);
+xlabel('Time(secs)');
+ylabel('Amplitude');
+title('Response to a Ramp Input');
+
+%cltf = feedback(k*sys1,1)
+%step(cltf/s)
+%ramp(cltf)
 
 %%Question 2
 num = [1];
